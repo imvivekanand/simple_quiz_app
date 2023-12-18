feeb44b9edfc53b015b06884ab522cdc8b4a3e20
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-12-18 23:42:57 +0530

    questions added

---
+++ script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "Particles which can be added to the nucleus of an atom without changing its chemical properties are",
+        answers: [
+            {text: "neutrons", correct: true},
+            {text: "electrons", correct: false},
+            {text: "protons", correct: false},
+            {text: "More than one of the above", correct: false},
+            {text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "The quality of a printer is determined as",
+        answers: [
+            {text: "words per inch", correct: false},
+            {text: "strike per inch", correct: false},
+            {text: "dots per inch", correct: true},
+            {text: "More than one of the above", correct: false},
+            {text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "Before Elon Musk announced change of the signature Blue Bird logo to Sign ‘X’ logo, who is regarded as the creator of Twitter’s iconic Bird in its final version in 2012?",
+        answers: [
+            {text: "Jack Dorsey", correct: false},
+            {text: "Martin Grasser", correct: true},
+            {text: "Noah Glass", correct: false},
+            {text: "More than one of the above", correct: false},
+            {text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "Who is the Director of the famous film Oppenheimer, a film about high-concept science, released this year?",
+        answers: [
+            {text: "Christopher Nolan", correct: true},
+            {text: "Guy Ritchie", correct: false},
+            {text: "Laura McGann", correct: false},
+            {text: "More than one of the above", correct: false},
+            {text: "None of the above", correct: false},
+        ]
+    },
+    {
+        question: "Which one of the following has pulled out from hosting the 2026 edition of Commonwealth Games due to huge estimated expenditure on its organization?",
+        answers: [
+            {text: "London, UK", correct: false},
+            {text: "Kuala Lumpur, Malaysia", correct: false},
+            {text: "Victoria, Australia", correct: true},
+            {text: "More than one of the above", correct: false},
+            {text: "None of the above", correct: false},
+        ]
+    },
+]
